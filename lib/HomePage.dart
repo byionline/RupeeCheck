57c02327d93a238ba90b55fa591ec50f6991fce8
byionline <10815566+byionline@users.noreply.github.com>
2020-06-29 22:58:58 +0530

    Added CircularLoading

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tflite/tflite.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,12 +7,32 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  bool _isLoading = false;
+  //File _image;
+  //List _output;
+
+  // App will work offline so model needs to load first
+  @override
+  void initState() {
+    // TODO: implement initState
+      super.initState();
+      _isLoading = true;
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Rupee Check"),
         backgroundColor: Colors.blueAccent,
+      ),
+      // Loading CircularProgress
+      body: _isLoading ? Container(
+        alignment: Alignment.center,
+        child: CircularProgressIndicator(),
+      ) : Container(
+
       ),
     );
   }
